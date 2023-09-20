7bea740a5f7ae30290c558abccba195607673716
Levon <levonbakunts3@gmail.com>
2023-09-21 01:20:14 +0400

    Create database, create authorization in react auth, permission, middleware

--- server/tour_agence/routes/web.php
+++ server/tour_agence/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Admin\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/admin_panel', [AdminController::class, "index"])->middleware(['role:admin']);
