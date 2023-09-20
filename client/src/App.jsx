2832bdb0d8d1db17e1cdf28176e5893bea89feb8
ArenDeveloper05 <antisystem005@mail.ru>
2023-09-20 16:31:03 +0400

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return <div className="App">
-    <h1>Client side for Phenix Corporation project</h1>
+    <h1>Client side for Phenix Corporation project.</h1>
   </div>;
 }
 
