0dd643412417aab1d40a8af49a4ca0de2b031407
Meri Hovhannisyan <meri.h1298@gmail.com>
2023-09-27 12:20:12 +0400

    delete form

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,11 +1,9 @@
-import Form from "./form/Form";
 import HomeSlider from "./home-slider/HomeSlider";
 
 const Home = () => {
   return (
     <div className="home">
         <HomeSlider />
-        <Form />
     </div>
   );
 };
